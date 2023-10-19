442c57b6b086ac548a586c7ab21fd0ddb6788554
cnmengkang <cnmengkang@gmail.com>
2023-10-19 21:01:15 +0800

    testaaa

--- vue-project/src/utils/websocket.js
+++ vue-project/src/utils/websocket.js
@@ -1,91 +1,104 @@
 // websocket
 
-import CryptoJS from 'crypto-js';
-import { base64 } from 'base64-js';
-const APPID = 'd9975a5a';
-const API_SECRET = 'ZWRhNWVlZTMyZTMyN2RlNjU3ZmMxMjVj';
-const API_KEY = '93644b74d4b368c48aa251e086c4a5cc';
+import CryptoJS from "crypto-js";
+import { base64 } from "base64-js";
+const APPID = "d9975a5a";
+const API_SECRET = "ZWRhNWVlZTMyZTMyN2RlNjU3ZmMxMjVj";
+const API_KEY = "93644b74d4b368c48aa251e086c4a5cc";
 const getWebSocketUrl = {
-    author() {
-        let apiKey = API_KEY;
-        let apiSecret = API_SECRET;
-        let url = 'wss://spark-api.xf-yun.com/v1.1/chat'
-        let host = location.host
-        let date = new Date().toUTCString();
-        let algorithm = 'hmac-sha256'
-        let headers = 'host date request-line'
-        let signatureOrigin = `host: ${host}\ndate: ${date}\nGET /v1.1/chat HTTP/1.1`
-        let signatureSha = CryptoJS.HmacSHA256(signatureOrigin, apiSecret)
-        let signature = CryptoJS.enc.Base64.stringify(signatureSha)
-        let authorizationOrigin = `api_key="${apiKey}", algorithm="${algorithm}", headers="${headers}", signature="${signature}"`
-        let authorization = btoa(authorizationOrigin)
-        url = `${url}?authorization=${authorization}&date=${date}&host=${host}`
-        return url;
-    }
-}
+  author() {
+    let apiKey = API_KEY;
+    let apiSecret = API_SECRET;
+    let url = "wss://spark-api.xf-yun.com/v1.1/chat";
+    let host = location.host;
+    let date = new Date().toUTCString();
+    let algorithm = "hmac-sha256";
+    let headers = "host date request-line";
+    let signatureOrigin = `host: ${host}\ndate: ${date}\nGET /v1.1/chat HTTP/1.1`;
+    let signatureSha = CryptoJS.HmacSHA256(signatureOrigin, apiSecret);
+    let signature = CryptoJS.enc.Base64.stringify(signatureSha);
+    let authorizationOrigin = `api_key="${apiKey}", algorithm="${algorithm}", headers="${headers}", signature="${signature}"`;
+    let authorization = btoa(authorizationOrigin);
+    url = `${url}?authorization=${authorization}&date=${date}&host=${host}`;
+    return url;
+  },
+};
 // WebSocketService.js
 
 class WebSocketService {
-    constructor(data) {
-        this.websocket = new WebSocket(getWebSocketUrl.author());
+  constructor(data) {
+    this.websocket = new WebSocket(getWebSocketUrl.author());
+    this.messageFragments = []; //处理消息分片的数据帧
+    this.chatMessage = [];
+    this.status = "";
+    this.websocket.onopen = () => {
+      console.log("WebSocket连接已打开");
+      this.WebSocketSend(data);
+    };
 
-        this.websocket.onopen = () => {
-            console.log('WebSocket连接已打开');
-            this.WebSocketSend(data)
-        };
+    this.websocket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      let format = data.payload.choices.text[0].content;
+      this.status = data.header.status;
+      this.messageFragments.push(format);
+      // 先检测消息完整性，如果消息完整直接处理？
+      if (this.messageIsComplete()) {
+        this.handleCompleteMessage();
+      }
+    };
+    this.websocket.onclose = () => {
+      console.log("WebSocket连接已关闭");
+    };
 
-        this.websocket.onmessage = (event) => {
-            const data = JSON.parse(event.data);
-            this.onMessage(data);
+    this.websocket.onerror = (error) => {
+      console.error("WebSocket发生错误:", error);
+    };
+  }
 
-        };
-
-        this.websocket.onclose = () => {
-            console.log('WebSocket连接已关闭');
-        };
-
-        this.websocket.onerror = (error) => {
-            console.error('WebSocket发生错误:', error);
-        };
-    }
-
-    onMessage(data) {
-        // 处理接收到的数据
-    }
-
-    // 发送数据包
-    WebSocketSend(message) {
-        let params = {
-            "header": {
-                "app_id": APPID,
-                "uid": "fd3f47e4-d"
+  appendMessage(data) {
+    // 处理接收到的数据
+  }
+  // 检测消息完整性
+  messageIsComplete() {
+    return this.status === 2 && this.messageFragments.length > 0;
+  }
+  // 合并数据以构建完整消息
+  handleCompleteMessage() {
+    console.log("合并与构建完整消息");
+    const combinedPayload = this.messageFragments.join("");
+    this.appendMessage(combinedPayload)
+  }
+  // 发送数据包
+  WebSocketSend(message) {
+    let params = {
+      header: {
+        app_id: APPID,
+        uid: "fd3f47e4-d",
+      },
+      parameter: {
+        chat: {
+          domain: "general",
+          temperature: 0.5,
+          max_tokens: 1024,
+        },
+      },
+      payload: {
+        message: {
+          text: [
+            {
+              role: "user",
+              content: message,
             },
-            "parameter": {
-                "chat": {
-                    "domain": "general",
-                    "temperature": 0.5,
-                    "max_tokens": 1024
-                }
-            },
-            "payload": {
-                "message": {
-                    "text": [
-
-                        {
-                            "role": "user",
-                            "content": message
-                        }
-                    ]
-                }
-            }
-        }
-        this.websocket.send(JSON.stringify(params));
-    }
-    // 手动关闭
-    close() {
-        this.websocket.close();
-    }
+          ],
+        },
+      },
+    };
+    this.websocket.send(JSON.stringify(params));
+  }
+  // 手动关闭
+  close() {
+    this.websocket.close();
+  }
 }
 
 export default WebSocketService;
-
--- vue-project/src/views/IndexView.vue
+++ vue-project/src/views/IndexView.vue
@@ -1,102 +1,85 @@
-
 <template>
-    <div class="container">
-        <div class="left-panel">
-            <ul>
-                <li :class="[item.role]" v-for="item in chatMessages" :key="item.id">
-                    {{ item.content }}
-                </li>
-            </ul>
-        </div>
-        <div class="right-panel">
-            <input v-model="inputText" @keyup.enter="sendMessage" />
-            <button @click="sendMessage">提交</button>
-        </div>
+  <div class="container">
+    <div class="left-panel">
+      <ul>
+        <li :class="[item.role]" v-for="item in chatMessages" :key="item.id">
+            {{ item.content }}
+        </li>
+      </ul>
     </div>
+    <div class="right-panel">
+      <input v-model="inputText" @keyup.enter="sendMessage" />
+      <button @click="sendMessage">提交</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import { ref, onUnmounted } from 'vue';
-import WebSocketService from '../utils/websocket';
+import { ref, onUnmounted } from "vue";
+import WebSocketService from "../utils/websocket";
 export default {
-    setup() {
-        const chatMessages = ref([]);
-        const inputText = ref('你好？');
-        const websocket = ref('');
-        const partialMessage = ref('')
+  setup() {
+    const chatMessages = ref([]);
+    const inputText = ref("你好？");
+    const websocket = ref("");
 
-        // 点击发送事件
-        // 用户数据
-        const sendMessage = () => {
-            const data = inputText.value;
-            chatMessages.value.push({ content: data, role: 'user' })
-            websocket.value = new WebSocketService(data);
-            websocket.value.onMessage = onMessage;
-            inputText.value = "";
-        };
-        // ai
-        const onMessage = (data) => {
-            let format = data.payload.choices.text;
-            format.forEach(reply => {
-                // 将分片消息逐步合并
-                partialMessage.value += reply.content + ' ';
-                // 如果消息是完整的，添加到chatMessages数组中
-                const combinedReply = {
-                    role: 'assistant',
-                    content: partialMessage.value,
-
-                }
-                chatMessages.value.push(combinedReply)
-                partialMessage.value = ''
-            })
-
-        };
-
-        onUnmounted(() => {
-            websocket.value.close();
-        });
-
-        return {
-            chatMessages,
-            inputText,
-            sendMessage,
-            partialMessage
+    // 点击发送事件
+    // 用户数据
+    const sendMessage = () => {
+      const data = inputText.value;
+      chatMessages.value.push({ content: data, role: "user" });
+      websocket.value = new WebSocketService(data);
+      websocket.value.appendMessage = appendMessage;
+      inputText.value = "";
+    };
+    // ai
+    const appendMessage = (data) => {
+      chatMessages.value.push({ role: "ai", content: data });
+    };
 
 
-        };
-    },
+    onUnmounted(() => {
+      websocket.value.close();
+    });
+
+    return {
+      chatMessages,
+      inputText,
+      sendMessage,
+    };
+  },
 };
 </script>
 
 <style>
 .container {
-    display: flex;
-    height: 100vh;
+  display: flex;
+  height: 100vh;
 }
 
 .left-panel {
-    flex: 1;
-    background-color: #f2f2f2;
-    padding: 20px;
-    overflow-y: auto;
+  flex: 1;
+  background-color: #f2f2f2;
+  padding: 20px;
+  overflow-y: auto;
 }
 
 .user {
-    color: red;
-    background-color: pink;
+  color: red;
+  background-color: pink;
 }
 
 .assistant {
-    text-align: left;
+  text-align: left;
 }
 
 .right-panel {
-    flex: 1;
-    background-color: #ffffff;
-    padding: 20px;
+  flex: 1;
+  background-color: #ffffff;
+  padding: 20px;
 }
 
 .message {
-    margin-bottom: 10px;
+  margin-bottom: 10px;
 }
 </style>
